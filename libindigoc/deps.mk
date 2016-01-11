06070c45a6c13ab3d6bd9a9ce6491ceef4375daa
Zik Saleeba <zik@zikzak.net>
2016-01-11 22:08:06 +1100

    Now builds lexer and parser correctly. Parser still lacks a link to the lexer.

--- libindigoc/deps.mk
+++ libindigoc/deps.mk
@@ -1,9 +1,11 @@
-/home/zik/obj/libindigoc/indigo.o: indigo.cpp indigo.h scheduler.h loadpackage.h pass.h
-/home/zik/obj/libindigoc/package.o: package.cpp package.h
-/home/zik/obj/libindigoc/pass.o: pass.cpp pass.h
-/home/zik/obj/libindigoc/sourcefile.o: sourcefile.cpp sourcefile.h
-/home/zik/obj/libindigoc/loadpackage.o: loadpackage.cpp loadpackage.h pass.h packagedb.h
-/home/zik/obj/libindigoc/packagedb.o: packagedb.cpp packagedb.h lmdb++.h \
- /home/zik/obj/libindigoc_gen/pdbfmt_generated.h
-/home/zik/obj/libindigoc/scheduler.o: scheduler.cpp scheduler.h pass.h
-/home/zik/obj/libindigoc/topleveldecl.o: topleveldecl.cpp topleveldecl.h
+/home/zik/obj/libindigoc/obj/indigo.o: indigo.cpp indigo.h scheduler.h loadpackage.h pass.h \
+ packagedb.h
+/home/zik/obj/libindigoc/obj/package.o: package.cpp package.h
+/home/zik/obj/libindigoc/obj/pass.o: pass.cpp pass.h
+/home/zik/obj/libindigoc/obj/sourcefile.o: sourcefile.cpp sourcefile.h
+/home/zik/obj/libindigoc/obj/sourcecode.o: sourcecode.cpp sourcecode.h
+/home/zik/obj/libindigoc/obj/loadpackage.o: loadpackage.cpp loadpackage.h pass.h packagedb.h
+/home/zik/obj/libindigoc/obj/packagedb.o: packagedb.cpp packagedb.h lmdb++.h \
+ /home/zik/obj/libindigoc/gen/pdbfmt_generated.h
+/home/zik/obj/libindigoc/obj/scheduler.o: scheduler.cpp scheduler.h pass.h
+/home/zik/obj/libindigoc/obj/topleveldecl.o: topleveldecl.cpp topleveldecl.h
